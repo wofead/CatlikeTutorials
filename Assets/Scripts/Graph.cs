e2b41d1778c35e2698f869973a703eade93c1af4
wofead <614667387@qq.com>
2020-10-15 18:33:53 +0800

    sync

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -15,22 +15,13 @@ public class Graph : MonoBehaviour
     {
         float step = 2f / resolution;
         Vector3 scale = Vector3.one * step;
-        Vector3 position = Vector3.zero;
         points = new Transform[resolution * resolution];
-        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        for (int i = 0; i < points.Length; i++)
         {
-            if (x == resolution)
-            {
-                x = 0;
-                z++;
-            }
             Transform point = Instantiate(pointPrefab);
-            points[i] = point;
             point.SetParent(transform, false);
-            position.x = (x + 0.5f) * step - 1f;
-            position.z = (z + 0.5f) * step - 1f;
-            point.localPosition = position;
             point.localScale = scale;
+            points[i] = point;
         }
     }
 
@@ -38,13 +29,19 @@ public class Graph : MonoBehaviour
     void Update()
     {
         float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
-        for (int i = 0; i < points.Length; i++)
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            position.y = f(position.x, position.z, time);
-            point.localPosition = position;
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
